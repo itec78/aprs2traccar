ca7c8a32ea92c6cf0ef317eee3f564e07b4da667
itec78 <itec78@gmail.com>
2022-06-10 17:27:50 +0200

    new features added

--- app/app.py
+++ app/app.py
@@ -9,9 +9,18 @@ import aprslib
 import geopy.distance
 import requests
 
+from apscheduler.schedulers.background import BlockingScheduler
+from datetime import datetime
+from requests.auth import HTTPBasicAuth
+import json
+import re
+from aprs2emoji import aprs2emoji
+
 DEFAULT_APRS_HOST = 'rotate.aprs.net'
-DEFAULT_TRACCAR_HOST = 'http://traccar:8082'
 DEFAULT_APRS_PORT = 14580
+DEFAULT_TRACCAR_HOST = 'http://traccar:8082'
+DEFAULT_TRACCAR_KEYWORD = 'aprs'
+DEFAULT_TRACCAR_INTERVAL = 60
 MSG_FORMATS = ['compressed', 'uncompressed', 'mic-e']
 
 LOGGER = logging.getLogger(__name__)
@@ -37,34 +46,41 @@ def gps_accuracy(gps, posambiguity: int) -> int:
     return accuracy
 
 
+
+
 class AprsListenerThread(threading.Thread):
     # APRS message listener.
 
-    def __init__(self, callsign: str, aprs_host: str, aprs_server_filter: str, traccar_host: str):
+    def __init__(self, aprs_callsign: str, aprs_host: str, aprs_filter_dict: dict, traccar_host: str):
         # Initialize the class.
         super().__init__()
 
-        self.callsign = callsign
-        self.aprs_server_filter = aprs_server_filter
+        self.aprs_callsign = aprs_callsign
+        self.aprs_filter_dict = aprs_filter_dict
         self.traccar_host = traccar_host
-
-        self.ais = aprslib.IS(self.callsign, host=aprs_host, port=DEFAULT_APRS_PORT)
+        self.ais = aprslib.IS(aprs_callsign, host=aprs_host, port=DEFAULT_APRS_PORT)
 
     def run(self):
         # Connect to APRS and listen for data.
-        self.ais.set_filter(self.aprs_server_filter)
+        self.setfilter(self.aprs_filter_dict)
 
         try:
-            LOGGER.info(f"Opening connection to APRS with callsign {self.callsign}.")
+            LOGGER.info(f"Opening connection to APRS with callsign {self.aprs_callsign}.")
             self.ais.connect()
             self.ais.consumer(callback=self.rx_msg, immortal=True)
         except OSError:
-            LOGGER.info(f"Closing connection to APRS with callsign {self.callsign}.")
+            LOGGER.info(f"Closing connection to APRS with callsign {self.aprs_callsign}.")
 
     def stop(self):
         # Close the connection to the APRS network.
         LOGGER.debug(f"stop()")
         self.ais.close()
+        # self.join()
+
+    def setfilter(self, aprs_filter_dict: dict):
+        self.aprs_filter_dict = aprs_filter_dict
+        filter = ("b/%s" % "/".join(sorted(list(aprs_filter_dict.keys()))))
+        self.ais.set_filter(filter)
 
     def tx_to_traccar(self, query: str):
         # Send position report to Traccar server
@@ -84,13 +100,13 @@ class AprsListenerThread(threading.Thread):
 
     def rx_msg(self, msg: dict):
         # Receive message and process if position.
-        LOGGER.info("APRS message received: %s", str(msg))
+        LOGGER.debug("APRS message received: %s", str(msg))
+
         if msg['format'] in MSG_FORMATS:
-            dev_id = msg['from']
             lat = msg['latitude']
             lon = msg['longitude']
 
-            query_string = f"id={dev_id}&lat={lat}&lon={lon}"
+            query_string = ""
 
             if 'posambiguity' in msg:
                 pos_amb = msg['posambiguity']
@@ -99,19 +115,87 @@ class AprsListenerThread(threading.Thread):
                 except ValueError:
                     LOGGER.warning(f"APRS message contained invalid posambiguity: {pos_amb}")
 
-            for attr in ['altitude', 'speed']:
+            for attr in ['altitude', 'speed', 'bearing']:
                 if attr in msg:
                     query_string += f"&{attr}={msg[attr]}"
 
-            #extra attributes
+            # extra attributes
             for attr in ['from', 'to', 'path', 'via', 'symbol', 'symbol_table', 'comment']:
                 if attr in msg:
                     query_string += f"&APRS_{attr}={msg[attr]}"
-            
-            try:
-                self.tx_to_traccar(query_string)
-            except ValueError:
-                logging.warning(f"id={dev_id}")
+
+            # icon
+            query_string += f"&APRS_icon=%s" % aprs2emoji(msg['symbol_table'],msg['symbol'])
+
+            dev_ids = self.aprs_filter_dict.get(msg['from'])
+            for dev_id in dev_ids:
+                query_fullstring = f"id={dev_id}&lat={lat}&lon={lon}" + query_string
+                try:
+                    self.tx_to_traccar(query_fullstring)
+                except ValueError:
+                    logging.warning(f"id={dev_id}")
+
+
+
+
+class Traccar2APRS():
+    def __init__(self,  TraccarHost: str, TraccarUser: str, TraccarPassword: str, TraccarKeyword: str, AprsCallsign: str, AprsHost: str):
+        # Initialize the class.
+        super().__init__()
+
+        self.TraccarHost = TraccarHost
+        self.TraccarUser = TraccarUser
+        self.TraccarPassword = TraccarPassword
+        self.TraccarKeyword = TraccarKeyword
+        self.AprsCallsign = AprsCallsign
+        self.AprsHost = AprsHost
+
+        self.ALT = None
+        self.lastfilterdict = []
+    
+    def poll(self):
+        page = requests.get(self.TraccarHost + "/api/devices?all=true", auth = HTTPBasicAuth(self.TraccarUser, self.TraccarPassword))
+        if page.status_code != 200:
+            LOGGER.info("Traccar auth failed")
+            return
+
+        filterdict={}
+        for j in json.loads(page.content):
+            # print(json.dumps(j, indent=2))
+            if not j["disabled"]:
+                attributes = j["attributes"]
+
+                for att, value in attributes.items():
+                    if re.search("^" + self.TraccarKeyword + "[0-9]{0,1}$", att.lower()):
+                        callsign = value.upper().strip()
+                        if re.search("^[A-Z]{1,2}[0-9][A-Z]{1,3}(-[A-Z0-9]{1,2}){0,1}$", callsign):
+                            unid = j["uniqueId"]
+                            filterdict[callsign] = filterdict.get(callsign, []) + [unid]
+
+        LOGGER.debug(f"Attributes: {filterdict}")
+
+        # check if it's running
+        if self.ALT is None or not self.ALT.is_alive():
+            if filterdict:
+                # if it's not running and must run, start it
+                self.ALT = AprsListenerThread(self.AprsCallsign, self.AprsHost, filterdict, self.TraccarHost)
+                self.ALT.start()   
+        else:
+            if filterdict:
+                # if it's running and must run, check the filter
+                if filterdict != self.lastfilterdict:
+                    self.ALT.setfilter(filterdict)
+
+            else:
+                # if it's running and mustn't run, stop it
+                self.ALT.stop()
+                self.ALT = None
+
+        self.lastfilterdict = filterdict
+    
+
+
+
 
 
 if __name__ == '__main__':
@@ -125,18 +209,27 @@ if __name__ == '__main__':
         logging.info("Exiting program.")
         exit(0)
 
-
     signal.signal(signal.SIGTERM, sig_handler)
     signal.signal(signal.SIGINT, sig_handler)
 
-    callsign = os.environ.get("CALLSIGN")
-    aprs_host = os.environ.get("APRS_HOST", DEFAULT_APRS_HOST)
-    filter = os.environ.get("APRS_FILTER", f"b/{callsign}")
     traccar_host = os.environ.get("TRACCAR_HOST", DEFAULT_TRACCAR_HOST)
+    traccar_user = os.environ.get("TRACCAR_USER", "")
+    traccar_password = os.environ.get("TRACCAR_PASSWORD", "")
+    traccar_keyword = os.environ.get("TRACCAR_KEYWORD", DEFAULT_TRACCAR_KEYWORD)
+    traccar_interval = int(os.environ.get("TRACCAR_INTERVAL", DEFAULT_TRACCAR_INTERVAL))
+    aprs_callsign = os.environ.get("APRS_CALLSIGN")
+    aprs_host = os.environ.get("APRS_HOST", DEFAULT_APRS_HOST)
 
-    if not callsign:
+    if not aprs_callsign:
         logging.fatal("Please provide your callsign to login to the APRS server.")
         exit(1)
 
-    ALT = AprsListenerThread(callsign, aprs_host, filter, traccar_host)
-    ALT.run()
+    T2A = Traccar2APRS(traccar_host, traccar_user, traccar_password, traccar_keyword, aprs_callsign, aprs_host)
+
+    logging.getLogger('apscheduler.executors.default').setLevel(logging.WARNING)
+    sched = BlockingScheduler()
+    sched.add_job(T2A.poll, 'interval', next_run_time=datetime.now(), seconds=traccar_interval)
+    sched.start()
+
+
+
